c3dc55aa39606807043a3cacf2ba2e27af4b17e7
Yehosuá Escobedo <93733677+YehosuaEs@users.noreply.github.com>
2025-02-23 23:39:04 +0100

    Squashed commit of the following:
    commit a7217d8c8709b894fdc99486f10a38a96140a09e
    Author: Yehosuá Escobedo <93733677+YehosuaEs@users.noreply.github.com>
    Date:   Sun Feb 23 23:26:36 2025 +0100
    
        test(app): create the new coverage with the new component forms and delete the unused one
    
    commit ec1b5c27b1beb369db91d3f24d1c95109932f544
    Author: Yehosuá Escobedo <93733677+YehosuaEs@users.noreply.github.com>
    Date:   Sat Feb 22 17:18:52 2025 +0100
    
        refactor(forms): create a unique dumb form and a form handler to reuse only one and inject the store in each component (#24)
    
        build(deps): update lock.json

--- src/app/features/form-handler/components/form/form.component.spec.ts
+++ src/app/features/form-handler/components/form/form.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ComponentRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { FormComponent } from './form.component';
+
+const formMock = new FormGroup({
+  name: new FormControl('', [Validators.required]),
+  description: new FormControl('', []),
+  status: new FormControl('', [])
+});
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let componentRef: ComponentRef<FormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +22,26 @@ describe('FormComponent', () => {
 
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+    componentRef.setInput('form', formMock);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit formSubmit when the form is valid', () => {
+    const emitSpy = spyOn(component.formSubmit, 'emit');
+
+    formMock.patchValue({
+      name: 'Project name',
+      description: 'Lorem ipsum',
+      status: 'Active'
+    });
+
+    component.onSubmit();
+
+    expect(emitSpy).toHaveBeenCalled();
   });
 });
